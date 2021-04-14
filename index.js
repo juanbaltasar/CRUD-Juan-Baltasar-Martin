495311a4c87923d610a8a28fd8f801a535b23673
juanbaltasar <martinjuanbaltasar@gmail.com>
2021-04-14 11:53:44 -0300

    Terminado

--- index.js
+++ index.js
@@ -10,19 +10,63 @@ const crud = function () {
     return {
         create: (name, lastname) => {
             /*YOUR CODE HERE */
-
+            const newItem = { id: autoincrement, name, lastname };
+            items.push(newItem)
+            autoincrement++;
+            return newItem
         },
         read: (id) => {
             /*YOUR CODE HERE */
+            let searchedElement = null;
 
+            let i = 0;
+
+            while (searchedElement == null && i < items.length) {
+                if (items[i].id == id) {
+                    searchedElement = items[i]
+                }
+                i++
+            }
+
+            return searchedElement
         },
         update: (id, name, lastname) => {
             /*YOUR CODE HERE */
+            //Se podria utilizar el metodo read para realizar el update, pero por cuestiones de modularizacion no lo utilizare
+            
+            let updatedElement = null;
 
+            let i = 0;
+
+            while (updatedElement == null && i < items.length) {
+                if (items[i].id == id) {
+                    updatedElement = { id, name, lastname }
+                    items[i] = updatedElement
+                }
+                i++
+            }
+
+            if (updatedElement == null) {
+                return false;
+            } else {
+                return updatedElement
+            }
         },
         delete: (id) => {
             /*YOUR CODE HERE */
+            let elementDeleted = false
 
+            let i = 0;
+
+            while (!elementDeleted && i < items.length) {
+                if (items[i].id == id) {
+                    items.splice(i, 1)
+                    elementDeleted = true
+                }
+                i++
+            }
+
+            return elementDeleted
         }
     }
 
